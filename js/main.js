eabab76347bc85f9034fda951f81540904f31576
Ekaterina Antonova <k.etekarina@gmail.com>
2021-09-30 13:53:52 +0300

    Доработана функция возвращения случайного числа, добавлены условия

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 // Пользовалась источником https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(min, max) {
+function getRandomPositiveIntInclusive(min, max) {
+  if (max < min) {
+    const tmp = min;
+    max = min;
+    min = tmp;
+  }
+
+  if (min < 0) {
+    min = 0;
+  }
+
+  if (max < 0) {
+    max = 0;
+  }
+
+  if (min === max) {
+    return min;
+  }
+
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomInt(1, 100);
+getRandomPositiveIntInclusive(1, 12);
 
 function checkCommentLength(comment, maxLength) {
   return comment.length <= maxLength;

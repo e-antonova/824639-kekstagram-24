b0b15ca6b343cd7b9cc093e1f5aab251ae8bba90
Ekaterina Antonova <k.etekarina@gmail.com>
2021-10-25 23:50:18 +0300

    Переделала цикл на forEach, удалила неиспользуемый код

--- js/render-thumbnails.js
+++ js/render-thumbnails.js
@@ -1,8 +1,8 @@
 // Модуль, который отвечает за отрисовку миниатюр
 import {createUserPhotos} from './data.js';
 
-const photosQuantity = 25;
-const photos = createUserPhotos(photosQuantity);
+const PHOTOS_QUANTITY = 25;
+const photos = createUserPhotos(PHOTOS_QUANTITY);
 
 const picturesContainer = document.querySelector('.pictures');
 const pictureFragment = document.querySelector('#picture').content;
@@ -11,19 +11,19 @@ const pictureTemplate = pictureFragment.querySelector('.picture');
 const renderThumbnails = () => {
   const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < photos.length; i++) {
+  photos.forEach((photo) => {
     const pictureItem = pictureTemplate.cloneNode(true);
 
     const pictureImg = pictureItem.querySelector('.picture__img');
     const pictureLikes = pictureItem.querySelector('.picture__likes');
     const pictureComments = pictureItem.querySelector('.picture__comments');
 
-    pictureImg.src = photos[i].url;
-    pictureLikes.textContent = photos[i].likes;
-    pictureComments.textContent = photos[i].comments.length;
+    pictureImg.src = photo.url;
+    pictureLikes.textContent = photo.likes;
+    pictureComments.textContent = photo.comments.length;
 
     fragment.appendChild(pictureItem);
-  }
+  });
 
   picturesContainer.appendChild(fragment);
 };
